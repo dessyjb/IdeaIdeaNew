5764735647e6c8ad4f2749925e76ead1b5919d58
Desi Boyanova <dessy_jb@yahoo.com>
2026-04-13 12:00:56 -0700

    Add IdeaIdeaNew files

---
+++ MyNewIdeaCenter/Models/ApiResponseDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json.Serialization;
+
+namespace MyNewIdeaCenter.Models
+{
+    internal class ApiResponseDTO
+    {
+        [JsonPropertyName("msg")]
+        public string Msg { get; set; }
+
+           [JsonPropertyName("id")]
+
+           public string ideaId { get; set; }
+
+
+    }
+}
